bfeee0412e1a2444850dda375946abb62da00ce3
benning55 <60070109@kmitl.ac.th>
2020-09-29 18:02:36 +0700

    add category two first cat

--- Mch_Level2_Senario/main.js
+++ Mch_Level2_Senario/main.js
@@ -8,8 +8,40 @@ const {
 } = require('appium-flutter-finder');
 const expect = require('chai').expect;
 
-const seeMainLevel2 = function(osts){
+const seeMainLevel2 = function(opts){
+
     describe('Check sub category', function(){
 
-    })
+        before(async function () {
+            this.timeout(50000 * 10000);
+            driver = await wdio.remote(opts);
+
+            usernameField = byValueKey('usernameTxt');
+            passwordField = byValueKey('passwordTxt');
+            loginButton = byValueKey('loginBtn');
+
+            await driver.elementSendKeys(usernameField, "551503");
+            await driver.elementSendKeys(passwordField, "551504");
+            await driver.elementClick(loginButton);
+            await driver.execute('flutter:waitForAbsent', loginButton);
+        });
+
+        it('Cathch first category', async function() {
+            this.timeout(30*1000);
+            await driver.elementClick(byValueKey('catThree0'));
+            categoryTwo = byValueKey("catTwo0");
+            console.log(categoryTwo);
+            expect(await driver.getElementText(categoryTwo)).to.equal('เครื่องใช้ไฟฟ้าในครัวขนาดเล็ก');
+        });
+
+        after(function () {
+            driver.deleteSession();
+        });
+
+    });
+}
+
+
+module.exports = {
+    seeMainLevel2
 }
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let loginTest = require('./Login_Senario/main');
 let Mchlevel3Test = require('./Mch_Level3_Senario/main');
+let Mchlevel2Test = require('./Mch_Level2_Senario/main')
 
 const osSpecificOps = {
     platformName: 'Android',
     deviceName: '4b283c1f9905',
     autoGrantPermissions: true,
-    app: 'C:\\Users\\bmais\\Documents\\SeniorHomepro\\flutter_ecatalog\\build\\app\\outputs\\apk\\dev\\debug\\ecatalog-dev-debug-vn_1_0_1-vc_1-20200929_135450.apk'
+    app: 'C:\\Users\\bmais\\Documents\\SeniorHomepro\\flutter_ecatalog\\build\\app\\outputs\\apk\\dev\\debug\\ecatalog-dev-debug-vn_1_0_1-vc_1-20200929_175638.apk'
     // @todo support non-unix style path
     // app: 'C:\\Users\\bmais\\Documents\\appium_flutter_test\\myapp\\build\\app\\outputs\\apk\\debug\\app-debug.apk'
     // app: '/home/benntend/Desktop/appium_flutter_test/myapp/build/app/outputs/apk/debug/app-debug.apk',
@@ -21,7 +22,8 @@ const opts = {
     }
 };
 
-loginTest.loginFail(opts);
-loginTest.loginPass(opts);
-Mchlevel3Test.seeMainLevel3(opts);
-Mchlevel3Test.searchBar(opts);
+// loginTest.loginFail(opts);
+// loginTest.loginPass(opts);
+// Mchlevel3Test.seeMainLevel3(opts);
+// Mchlevel3Test.searchBar(opts);
+Mchlevel2Test.seeMainLevel2(opts);
